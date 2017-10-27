bc84a2364b8c7c83f51977a38c1f79e8d359dc10
Cheulong Seaer <searcheulong@gmail.com>
2017-10-27 15:03:11 +0700

    4.6

--- src/main/java/camt/cbsd/lab05/repository/StudentRespository.java
+++ src/main/java/camt/cbsd/lab05/repository/StudentRespository.java
@@ -4,4 +4,5 @@ import camt.cbsd.lab05.entity.Student;
 import org.springframework.data.repository.CrudRepository;
 
 public interface StudentRespository extends CrudRepository<Student,Long> {
+    Student findById(long id);
 }
